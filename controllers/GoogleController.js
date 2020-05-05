f0e69b3b576b18878aa2edd27aad13c5f77a7bc3
achramadhans <littleworkshop.id@gmail.com>
2020-05-06 02:55:13 +0700

    'test'

--- controllers/GoogleController.js
+++ controllers/GoogleController.js
@@ -3,52 +3,58 @@ const { User } = require('../models')
 const { generateToken } = require('../helpers/jwt')
 
 class GoogleController {
-    static googleLogin(req, res, next) {
-        const client = new OAuth2Client(process.env.CLIENT_ID);
-        let email = ''
-        client.verifyIdToken({
-                idToken: req.headers.id_token,
-                audience: process.env.CLIENT_ID
-            })
-            .then(ticket => {
-                email = ticket.getPayload().email
-                return User.findOne({
-                    where: {
-                        email
-                    }
-                })
-            })
-            .then(data => {
-                if (data) {
-                    let payload = {
-                        id: data.id,
-                        email: data.email
-                    }
-                    let token = generateToken(payload)
-                    return res.status(200).json({
-                        'access_token': token
-                    })
-                } else {
-                    return User.create({
-                        email,
-                        password: process.env.DEFAULT_PASSWORD
-                    })
-                }
-            })
-            .then(data => {
-                let payload = {
-                    id: data.id,
-                    email: data.email
-                }
-                let token = generateToken(payload)
-                return res.status(201).json({
-                    'access_token': token
-                })
-            })
-            .catch(err => {
-                return next(err)
-            })
-    }
+  static googleSign(req, res, next) {
+    const client = new OAuth2Client(process.env.CLIENT_ID);
+    let email = ''
+    console.log(req.body.id_token)
+    client.verifyIdToken({
+        idToken: req.body.id_token,
+        audience: process.env.CLIENT_ID
+      })
+      .then(ticket => {
+        console.log(ticket)
+        email = ticket.getPayload().email
+        return User.findOne({
+          where: {
+            email: email
+          }
+        })
+      })
+      .then(data => {
+        if (data) {
+          let user = {
+            id: data.id,
+            email: data.email
+          }
+          let token = generateToken(user)
+          return res.status(200).json({
+            id: user.id,
+            email: user.email,
+            access_token: token
+          })
+        } else {
+          return User.create({
+            email: email,
+            password: process.env.DEFAULT_PASSWORD
+          })
+        }
+      })
+      .then(data => {
+        let user = {
+          id: data.id,
+          email: data.email
+        }
+        let token = generateToken(user)
+        return res.status(201).json({
+          id: user.id,
+          email: user.email,
+          access_token: token
+        })
+      })
+      .catch(err => {
+        next(err)
+      })
+  }
 }
 
 module.exports = GoogleController
