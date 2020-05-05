c25bd5669bccf499ee360fa6c5bdd08a112e7ab8
achramadhans <littleworkshop.id@gmail.com>
2020-05-05 19:12:02 +0700

    'heroku'

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,54 +3,54 @@ const { generateToken } = require('../helpers/jwt')
 const { User } = require('../models')
 
 class UserController {
-    static register(req, res, next) {
-        let { email, password, verifypassword } = req.body
-        console.log({ email, password, verifypassword });
-        if (password == verifypassword) {
-            User.create({
-                    email: email,
-                    password: password,
-                    createdAt: new Date(),
-                    updatedAt: new Date()
-                })
-                .then(data => {
-                    console.log(data)
-                    let payload = { id: data.id, email: data.email }
-                    let access_token = generateToken(payload)
-                    console.log(access_token);
-                    return res.status(200).json({
-                        'access_token': access_token
-                    })
-                })
-                .catch(err => {
-                    return next(err)
-                })
-        } else return next({
-            name: 'BadRequest',
-            msg: 'Verify password missmatch'
+  static register(req, res, next) {
+    let { email, password, verifypassword } = req.body
+    console.log({ email, password, verifypassword });
+    if (password == verifypassword) {
+      User.create({
+          email: email,
+          password: password,
+          createdAt: new Date(),
+          updatedAt: new Date()
         })
-    }
+        .then(data => {
+          console.log(data)
+          let payload = { id: data.id, email: data.email }
+          let access_token = generateToken(payload)
+          console.log(access_token);
+          return res.status(200).json({
+            'access_token': access_token
+          })
+        })
+        .catch(err => {
+          next(err)
+        })
+    } else next({
+      name: 'BadRequest',
+      msg: 'Verify password missmatch'
+    })
+  }
 
-    static login(req, res, next) {
-        let { email, password } = req.body
-        User.findOne({ where: { email: email } })
-            .then(data => {
-                if (data) {
-                    let decrypted = compare(password, data.password)
-                    if (decrypted) {
-                        let payload = { id: data.id, email: data.email }
-                        let access_token = generateToken(payload)
-                        return res.status(200).json({
-                            'access_token': access_token
-                        })
-                    } else return next({ name: 'BadRequest', msg: 'Wrong email/password' })
+  static login(req, res, next) {
+    let { email, password } = req.body
+    User.findOne({ where: { email: email } })
+      .then(data => {
+        if (data) {
+          let decrypted = compare(password, data.password)
+          if (decrypted) {
+            let payload = { id: data.id, email: data.email }
+            let access_token = generateToken(payload)
+            return res.status(200).json({
+              'access_token': access_token
+            })
+          } else next({ name: 'BadRequest', msg: 'Wrong email/password' })
 
-                } else return next({ name: 'BadRequest', msg: 'Wrong email/password' })
-            })
-            .catch(err => {
-                return next({ name: 'NotFound', msg: 'User Not found' })
-            })
-    }
+        } else next({ name: 'BadRequest', msg: 'Wrong email/password' })
+      })
+      .catch(err => {
+        next({ name: 'NotFound', msg: 'User Not found' })
+      })
+  }
 }
 
 module.exports = UserController
