1b24b788cd6295a02ecf73e5dc66ec0d70b5e478
achramadhans <littleworkshop.id@gmail.com>
2020-05-05 18:42:52 +0700

    'heroku'

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config()
+const PORT = process.env.PORT || 3000
+const express = require("express")
+const cors = require('cors')
+const app = express()
+const routes = require('./routes')
+
+app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/', routes)
+app.listen(PORT, () => {
+    console.log('Server Port : ' + PORT)
+})
---
+++ models/kanban.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+
+    const { Model } = sequelize.Sequelize
+
+    class Kanban extends Model {}
+
+    Kanban.init({
+        title: DataTypes.STRING,
+        description: DataTypes.STRING,
+        tag: DataTypes.STRING,
+        userId: DataTypes.INTEGER
+    }, {
+        hooks: {
+            beforeCreate: (Kanban, options) => {
+                Kanban.tag = 'new entry'
+            }
+        },
+        sequelize
+    });
+
+    Kanban.associate = function(models) {
+        Kanban.belongsTo(models.User, { foreignKey: 'userId' })
+    };
+    return Kanban;
+};
