ae55e14667bddea674e2132551d8c949f2b55602
achramadhans <littleworkshop.id@gmail.com>
2020-05-06 02:56:35 +0700

    'test'

--- routes/user.js
+++ routes/user.js
@@ -3,5 +3,5 @@ const UserController = require('../controllers/UserController')
 const GoogleController = require('../controllers/GoogleController')
 router.post('/login', UserController.login)
 router.post('/register', UserController.register)
-router.post('/googleLogin', GoogleController.googleLogin)
+router.post('/googleLogin', GoogleController.googleSign)
 module.exports = router
