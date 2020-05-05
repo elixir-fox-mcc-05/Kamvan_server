26272d38077f7ee4b1df00d01816811e8cd04a6e
achramadhans <littleworkshop.id@gmail.com>
2020-05-05 19:11:24 +0700

    'heroku'

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const express = require("express")
 const cors = require('cors')
 const app = express()
 const routes = require('./routes')
+const errorHandler = require('./middlewares/errorHandler')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+app.use(errorHandler)
 app.use('/', routes)
+
 app.listen(PORT, () => {
   console.log('Server Port : ' + PORT)
 })
