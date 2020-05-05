36f86d119466f3a62cd2994e7b3a43681c75ec61
achramadhans <littleworkshop.id@gmail.com>
2020-05-06 00:02:27 +0700

    'fix'

--- models/kanban.js
+++ models/kanban.js
@@ -1,26 +1,26 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
 
-    const { Model } = sequelize.Sequelize
+  const { Model } = sequelize.Sequelize
 
-    class Kanban extends Model {}
+  class Kanban extends Model {}
 
-    Kanban.init({
-        title: DataTypes.STRING,
-        description: DataTypes.STRING,
-        tag: DataTypes.STRING,
-        userId: DataTypes.INTEGER
-    }, {
-        hooks: {
-            beforeCreate: (Kanban, options) => {
-                Kanban.tag = 'new entry'
-            }
-        },
-        sequelize
-    });
+  Kanban.init({
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    tag: DataTypes.STRING,
+    userId: DataTypes.INTEGER
+  }, {
+    hooks: {
+      beforeCreate: (Kanban, options) => {
+        Kanban.tag = 'backlog'
+      }
+    },
+    sequelize
+  });
 
-    Kanban.associate = function(models) {
-        Kanban.belongsTo(models.User, { foreignKey: 'userId' })
-    };
-    return Kanban;
+  Kanban.associate = function(models) {
+    Kanban.belongsTo(models.User, { foreignKey: 'userId' })
+  };
+  return Kanban;
 };
