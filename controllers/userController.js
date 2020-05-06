accc5adec93937b0bcf57b98650c58b1ef26b4a6
yaw96_ <yaw.networks@gmail.com>
2020-05-06 21:40:34 +0700

    add google Oauth

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const Model = require('../models/index.js');
 const User = Model.User;
 const { checkPassword } = require('../helpers/bcryptjs.js');
 const { generateToken } = require('../helpers/jsonwebtoken.js');
+const googleVerification = require('../helpers/googleOauth.js');
 
 class UserController {
 	static signup(req, res, next) {
@@ -54,6 +55,48 @@ class UserController {
 						message: 'Opps!, invalid email / password'
 					};
 				}
+			})
+			.catch((err) => {
+				next(err);
+			});
+	}
+
+	static googleLogin(req, res, next) {
+		let google_token = req.headers.google_token;
+		let email = null;
+		let newUser = null;
+
+		googleVerification(google_token)
+			.then((dataPayload) => {
+				email = dataPayload.email;
+
+				let options = {
+					where: { email }
+				};
+				return User.findOne(options);
+			})
+			.then((dataUser) => {
+				if (dataUser) {
+					return dataUser;
+				} else {
+					newUser = true;
+					return User.create({
+						email: email,
+						password: process.env.DEFAULT_PASSWORD_GOOGLE_LOGIN
+					});
+				}
+			})
+			.then((user) => {
+				let code = newUser ? 201 : 200;
+
+				let token = generateToken({
+					id: user.id,
+					email: user.email
+				});
+
+				res.status(code).json({
+					token
+				});
 			})
 			.catch((err) => {
 				next(err);
