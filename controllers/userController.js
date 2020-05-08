e63161ce58174c420c6c4da073311127860e594f
AndreRGunawan <andre.graphica@gmail.com>
2020-05-08 22:44:16 +0700

    first deploy attempt

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,7 +20,6 @@ class userController {
             })
           })
           .catch(error =>{
-              console.log(error)
               res.status(500).json({
                   message: "Internal server error",
                   error
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ var router = express.Router()
 const userRoutes = require("../routes/userRoutes.js")
 const taskRoutes = require("../routes/taskRoutes.js")
 
+router.get('/', function(req, res) {
+    res.status(200).json({
+        message: 'Home Domain Connected'
+    })
+})
 router.use('/', userRoutes)
 router.use('/tasks', taskRoutes)
 
