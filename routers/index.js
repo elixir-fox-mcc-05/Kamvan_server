ee17a91940e471e1bfe6dea48abd096057909f10
Ottoyd <ugaipribadi@gmail.com>
2020-05-08 20:43:48 +0700

    'sleep'

--- routers/index.js
+++ routers/index.js
@@ -1,9 +1,7 @@
 let router = require('express').Router()
 let userRouter = require('./user')
 let taskRouter = require('./task')
-router.get('/',(req,res) => {
-    res.json({msg: 'welcome home'})
-})
+
 router.use('/user', userRouter)
 router.use('/task', taskRouter)
 
--- routers/task.js
+++ routers/task.js
@@ -1,10 +1,12 @@
 let router = require('express').Router()
 let TaskController = require('../controllers/task')
 let {authentification} = require('../middlewares/authentification')
+let {authorization} = require('../middlewares/authorization')
 
 router.get('/', TaskController.show)
 router.use(authentification)
 router.post('/', TaskController.create)
+router.use(authorization)
 router.patch('/:id', TaskController.step)
 router.delete('/:id', TaskController.delete)
 
