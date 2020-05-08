8e5d6cc0001a3b2776616eacaccb2079e8e2ec0f
Ottoyd <ugaipribadi@gmail.com>
2020-05-08 17:00:03 +0700

    'dsf'

--- routers/index.js
+++ routers/index.js
@@ -1,7 +1,9 @@
 let router = require('express').Router()
 let userRouter = require('./user')
 let taskRouter = require('./task')
-
+router.get('/',(req,res) => {
+    res.json({msg: 'welcome home'})
+})
 router.use('/user', userRouter)
 router.use('/task', taskRouter)
 
