e13bbea4342fdc725a1cb36395a131ccf807ca60
adityapradipta <adityapradipta94@gmail.com>
2020-05-08 16:25:22 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
 const app = express()
 const router = require('./routes/index.js');
 const port = process.env.PORT
 const errHandler = require('./middlewares/errHandler')
 const cors = require('cors')
-// const path = require('path');
 
 app.use(cors())
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 app.use(router)
