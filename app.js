b97a2e318036ae8b749d125d348e5d8fdbce3f71
yaw96_ <yaw.networks@gmail.com>
2020-05-08 19:16:40 +0700

    deploy

--- app.js
+++ app.js
@@ -9,6 +9,16 @@ const errHandler = require('./middlewares/errHandler.js');
 const app = express();
 const port = process.env.PORT || 3000;
 
+let sequelize;
+if (config.use_env_variable) {
+	sequelize = new Sequelize(process.env[config.use_env_variable], {
+		dialect: 'postgres',
+		protocol: 'postgres'
+	});
+} else {
+	sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
