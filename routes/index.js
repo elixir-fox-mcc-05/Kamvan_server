a34f37686fa40d1e2b6cc15b5c64fa9a1171f0f9
azka-art <59497414+azka-art@users.noreply.github.com>
2020-05-12 13:13:46 +0700

    edited

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const usersRoutes = require('./users')
 const tasksRoutes = require('./tasks')
 
-router.get('/', (req, res) => res.status(200).json({ msg: "Successfully connected to server" }))
+router.get('/', (req, res) => res.status(200).json(res))
 router.use('/users', usersRoutes)
 router.use('/tasks', tasksRoutes)
 
