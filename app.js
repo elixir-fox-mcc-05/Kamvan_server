26d3ed72fced335ab80d48284eb50c42b153de61
amirfaisalz <amirsakti27@gmail.com>
2020-05-24 22:50:13 +0700

    try deploy again

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ if (process.env.Node_ENV == "development") {
 
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const routes = require("./routes/index");
 const errorHandler = require("./middlewares/errorHandler");
 const cors = require("cors");
