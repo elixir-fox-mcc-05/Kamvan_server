a45b3c1da0c742c79b3be319ae6fd2ad05b6c42c
Grady Wicoady <nervalgravie@gmail.com>
2020-05-28 10:27:11 +0800

    first commit

--- router/index.js
+++ router/index.js
@@ -3,7 +3,7 @@ const UserController = require('../controllers/userController')
 const task = require('./task')
 
 router.use('/tasks',task)
-router.get('/login',UserController.login)
+// router.get('/login',UserController.login)
 router.post('/login',UserController.login)
 router.post('/register', UserController.register)
 router.post('/googlelogin', UserController.googleLogin)
